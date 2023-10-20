5adb70db3359f6baff743f678a79c9645236139b
EdenBendheim <edenbendheim@gmail.com>
2023-10-20 13:58:40 -0400

    quiz 1

---
+++ quiz1/readme.md
@@ -0,0 +1,6 @@
+**Eden Bendheim README**
+Link: bendherpi924276.eastus.cloudapp.azure.com/iit/quiz1
+While creating the API call I ran into a couple issues.
+The first one was when I was trying to output a hardcoded value and I was not able to. 
+The issue was that I never closed my select element, so it wasnt showing up.
+The way I got around not having a button to change the output was to just have stuff change on the input. The input is USD, and the output is decided by the dropdown. This was not that bad to make, since I just used onclick for the dropdown, and it changes when you type something by using onkeydown.
---
+++ quiz1/script.js
@@ -0,0 +1,137 @@
+// Define a function to fetch weather data
+function getWeatherData() {
+    // Replace 'YOUR_API_KEY' with your actual API key
+    var apiKey = '77ea4d1bad3baf1c704ab1035099e3c0';
+    // var city = 'New York'; // Replace with the desired city
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=42.7284&lon=-73.6918&appid=' + apiKey;
+    // city + '&appid=' + apiKey;
+
+    $.ajax({
+        url: apiUrl,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            // Call another function to parse and display the weather information
+            getInfo(data);
+        },
+        error: function(error) {
+            console.error('Error:', error);
+        }
+    });
+}
+$(document).ready(function() {
+    // Define a function to fetch data about a random Game of Thrones character
+    function getRandomCharacter() {
+        $.ajax({
+            url: 'https://anapioficeandfire.com/api/characters/' + Math.floor(Math.random() * 2138), // There are 2138 characters in the API
+            method: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                // Call a function to display the character information
+                displayCharacterInfo(data);
+            },
+            error: function(error) {
+                console.error('Error fetching character:', error);
+            }
+        });
+    }
+
+    // Define a function to display character information
+function displayCharacterInfo(character) {
+    // Helper function to handle empty categories
+    function handleEmptyCategory(category) {
+        return category.trim() !== '' ? category : 'Not Available';
+    }
+
+    var characterInfo = `
+        <h2>${character.name}</h2>
+        <p>Gender: ${handleEmptyCategory(character.gender)}</p>
+        <p>Culture: ${handleEmptyCategory(character.culture)}</p>
+        <p>Aliases: ${character.aliases.length > 0 ? character.aliases.join(', ') : 'Not Available'}</p>
+        <p>Born: ${handleEmptyCategory(character.born)}</p>
+        <p>Died: ${handleEmptyCategory(character.died)}</p>
+    `;
+
+    $('#character-info').html(characterInfo);
+}
+
+
+    // Trigger the random character fetch when the document is ready
+    getRandomCharacter();
+});
+
+function convert(){
+    var from = 'USD';
+    var to = $('#to-currency').val();
+    var amount = $('#amount').val();
+    console.log(amount);
+
+    $.ajax({
+        // Get the value of currency in the dropdown menu
+        url: `https://api.frankfurter.app/latest?=`+ amount +`&from=USD&to=`+ to,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            // Call another function to parse and display the weather information
+            getMoney(data);
+        },
+        error: function(error) {
+            console.error('Error:', error);
+        }
+    });
+}
+function getMoney(data) {
+    var to = $('#to-currency').val();
+    var new_val = data.rates[to];
+    var value = $('#amount').val() * new_val;
+    var amount = $('#amount').val();
+
+    var moneyInfo = `<p> ${amount} USD = ${value} `+ to +` </p>`;
+    
+    $('#money-info').html(moneyInfo);
+}
+
+
+// Define a function to parse and display weather information
+// Define a function to parse and display weather information
+function getInfo(data) {
+    var city = data.name;
+    var temperature = (data.main.temp - 273.15).toFixed(2); // Convert from Kelvin to Celsius
+    var feelsLike = (data.main.feels_like - 273.15).toFixed(2);
+    var condition = data.weather[0].description;
+    var humidity = data.main.humidity;
+    var windSpeed = data.wind.speed;
+    var windDirection = data.wind.deg;
+    var pressure = data.main.pressure;
+    var visibility = data.visibility;
+    var sunriseTime = new Date(data.sys.sunrise * 1000); // Convert Unix timestamp to a JavaScript date
+    var sunsetTime = new Date(data.sys.sunset * 1000);
+    var weatherIcon = data.weather[0].icon;
+    var weatherId = data.weather[0].id;
+
+    var weatherInfo = `
+        <h2>${city} Weather</h2>
+        <p id="temp">Temperature: ${temperature}°C</p>
+        <img id="image"src="https://openweathermap.org/img/wn/${weatherIcon}.png" alt="Weather Icon">
+        <p>Feels Like: ${feelsLike}°C</p>
+        <p>Condition: ${condition}</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} km/h</p>
+        <p>Wind Direction: ${windDirection}°</p>
+        <p>Pressure: ${pressure} hPa</p>
+        <p>Visibility: ${visibility} meters</p>
+        <p>Sunrise Time: ${sunriseTime.toLocaleTimeString()}</p>
+        <p>Sunset Time: ${sunsetTime.toLocaleTimeString()}</p>
+        <p>Weather ID: ${weatherId}</p>
+    `;
+    
+
+    $('#weather-info').html(weatherInfo);
+}
+
+
+// Call the function to fetch weather data when the document is ready
+$(document).ready(function() {
+    getWeatherData();
+    convert();
+});
